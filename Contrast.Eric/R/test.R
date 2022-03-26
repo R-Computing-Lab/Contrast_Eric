6f6d38e465e2cb35018e33bca913557a048ef6b1
Mason Garrison <garrissm@wfu.edu>
2022-03-26 14:51:45 -0400

    minor style standardization
    I did some minor style standardization for saving future us some headaches

--- Contrast.Eric/R/test.R
+++ Contrast.Eric/R/test.R
@@ -1,5 +1,9 @@
-test <- data.frame(Lev = as.factor(c("A","B","C","A","B","C")),
-                   Val = 1:6)
-Coe.Matrix.one(test$Lev,
-               c("A","B", "C"),
-               matrix(c(1,-0.5,-0.5), nrow = 1, ncol = 3))
+test <- data.frame(
+  lev = as.factor(c("A", "B", "C", "A", "B", "C")),
+  val = 1:6
+)
+coe_matrix_one(
+  test$lev,
+  c("A", "B", "C"),
+  matrix(c(1, -0.5, -0.5), nrow = 1, ncol = 3)
+)
