c63bef255004804c6b604bf07aafc1c2f8de404a
Xuanyu Lyu <lxy750909935@gmail.com>
2022-03-31 15:31:14 -0400

    The function to generate the coefficient matrix is done! It is not limited to the number of factors.

--- contrast.eric/R/coe_matrix.R
+++ contrast.eric/R/coe_matrix.R
@@ -1,66 +1,75 @@
-#### A very first trial for the package, let's say a contrast for a 3-group variable
+#### A function to generate coefficients matrix
 
 
 #### In order to make your hypotheses to be correctly converted to a coefficient matrix, you need to follow the rules below:
-#### levels: a character vector indicating which levels of the factor your hypotheses are related to
-#### coe: a numeric matrix indicating what the coefficients for each of your hypotheses, corresponding to the character vector in levels
-#### col_factor:
-
-coe_matrix_one <- function(col_factor, levels, coe) {
-  ## Build a coefficient matrix for the selected levels
-  select_levels <- as.character(levels)
-  select_coe_matrix <- as.data.frame(coe)
-  colnames(select_coe_matrix) <- select_levels
-  ## Build a coefficient matrix for  all levels of the factor
-  all_levels <- as.character(unique(col_factor))
-  coe_matrix <- as.data.frame(matrix(
-    data = 0,
-    ncol = length(all_levels),
-    nrow = nrow(select_coe_matrix)
-  ))
-  colnames(coe_matrix) <- all_levels
-
-  ## Put the selected coefficient matrix into the full coefficient matrix
-  for (i in 1:ncol(select_coe_matrix)) {
-    tempcol <- colnames(select_coe_matrix)[i]
-    coe_matrix[tempcol] <- select_coe_matrix[tempcol]
-  }
-  return(coe_matrix)
-}
-
-#### Let's try more than one factor, say a 2X2 design
+#### levels: a list of character vectors indicating which levels of the factor your hypotheses are related to
+#### coe: a list of numeric vectors indicating what the coefficients for each of your hypotheses, corresponding to the character vector in levels
+#### col_factor: The columns of all different factors involved in the testing.
 
 coe_matrix_multi <- function(colFactor, levels, coe){
-      numFactor <- ncol(colFactor)
-      if(numFactor==1){
-        coe_matrix_one(colFactor, levels, coe)
+      colFactor <- as.data.frame(colFactor)
+      ## Create the empty coefficients table
+      numFactor <- ifelse(is.null(ncol(colFactor)), 1, ncol(colFactor))
+      l <- list()
+      for (i in 1:numFactor) {
+        l[[i]] <- as.character(unique(colFactor[,i]))
       }
+      allcomb <- expand.grid(l)
+      #print(allcomb)
 
-      ## try to write a function to use recursion to get all possible combination
-      combn_recur <- function(colFactor){
-        numFactor2 <- ncol(colFactor)
-        allcol <- colFactor
-        if(numFactor2 > 1){
-
-          numFactor2 = numFactor2 - 1
-          combn_recur(allcol)
-
-          return()
+      all_levels <- character()
+      if(ncol(allcomb)==1){
+        for(i in 1:nrow(allcomb)){
+          all_levels[i] <- as.character(allcomb[i,1])
         }
-        if(numFactor2 == 1){
-          return()
+      }else{
+        for(i in 1:nrow(allcomb)){
+          all_levels[i] <- do.call(paste, c(allcomb[i,], sep = "X"))
         }
       }
 
+      #print(all_levels)
 
+      coe_matrix <- as.data.frame(matrix(
+        data = 0,
+        ncol = length(all_levels),
+        nrow = length(coe)
+      ))
+      #print(coe_matrix)
+      colnames(coe_matrix) <- all_levels
 
-      if(numFactor>1){
-            for (i in 1:numFactor) {
-                  for (j in 1:length(unique(colFactor[i]))) {
-
-                  }
-            }
+      ## put the selected coefficient list into the full coefficient matrix
+      for (i in 1: length(levels)){
+        #temp_coe_matrix <- matrix(coe[i], nrow = 1, ncol = length(coe[i]) )
+        #tempcol <- levels[i]
+        coe_matrix[i,][levels[[i]]] <- coe[[i]]
       }
 
-      combn()
+      return(coe_matrix)
 }
+
+
+### Ignore the function below: one level codes, which have been combined into the new function
+# coe_matrix_one <- function(col_factor, levels, coe) {
+#   ## Build a coefficient matrix for the selected levels
+#   select_levels <- as.character(levels)
+#   select_coe_matrix <- as.data.frame(coe)
+#   colnames(select_coe_matrix) <- select_levels
+#   ## Build a coefficient matrix for  all levels of the factor
+#   all_levels <- as.character(unique(col_factor))
+#   coe_matrix <- as.data.frame(matrix(
+#     data = 0,
+#     ncol = length(all_levels),
+#     nrow = nrow(select_coe_matrix)
+#   ))
+#   colnames(coe_matrix) <- all_levels
+#
+#   ## Put the selected coefficient matrix into the full coefficient matrix
+#   for (i in 1:ncol(select_coe_matrix)) {
+#     tempcol <- colnames(select_coe_matrix)[i]
+#     coe_matrix[tempcol] <- select_coe_matrix[tempcol]
+#   }
+#   return(coe_matrix)
+# }
+
+#### Let's try more than one factor, say a 2X2 design
--- contrast.eric/R/test.R
+++ contrast.eric/R/test.R
@@ -1,5 +1,7 @@
 test <- data.frame(
   lev = as.factor(c("A", "B", "C", "A", "B", "C")),
+  lev2 = as.factor(c("x", "y", "z","x", "y", "z")),
+  lev3 = as.factor(c("O", "M", "N","O", "M", "N")),
   val = 1:6
 )
 coe_matrix_one(
@@ -7,3 +9,27 @@ coe_matrix_one(
   c("A", "B", "C"),
   matrix(c(1, -0.5, -0.5), nrow = 1, ncol = 3)
 )
+
+
+testl <- list(c("AXyXO", "CXxXN", "BXyXN"),c("CXyXO", "CXxXM", "BXzXN", "AXzXM"))
+coe_matrix_multi(test[,1:3],
+                 list(c("AXyXO", "CXxXN", "BXyXN"),
+                      c("CXyXO", "CXxXM", "BXzXN", "AXzXM")),
+                 list(c(1,-0.5,-0.5),
+                      c(-1,-1,1,1)))
+length(testl)
+
+
+do.call(paste, c(test[1,], sep = "X"))
+
+coe_matrix_multi(test[,1:2],
+                 list(c("AXy", "CXx", "BXy"),
+                      c("CXy", "CXx", "BXz", "AXz")),
+                 list(c(1,-0.5,-0.5),
+                      c(-1,-1,1,1)))
+
+coe_matrix_multi(test[,1],
+                 list(c("A", "C", "B"),
+                      c("C", "B", "A")),
+                 list(c(1,-0.5,-0.5),
+                      c(-1,1,1)))
